19ceed2c7a5911a11887ed510b33b6e4346a70e4
sanWenyua <mrkangone@hotmail.com>
2018-01-09 13:46:03 +0800

    edit

--- judger_server/server.py
+++ judger_server/server.py
@@ -32,7 +32,34 @@ class InitSubmissionEnv(object):
 
 
 class JudgerServer(object):
-	def judge(self):
+	def judge(self,language_config,src,max_cpu_time,max_memory,test_case_id,
+			spj_version=None,spj_config=None,spj_compile_config=None,spj_src=None,
+			output=False):
+
+		compile_config=language_config.get("compile")
+		run_config=language_config["run"]
+		submission_id=str(uuid.uuid4())
+
+		if spj_config and spj_version:
+			spj_exe_path=os.path.join(SPJ_EXE_DIR,spj_config["name"].format(spj_version=spj_version))
+			if not os.path.isfile(spj_exe_path):
+				logger.warning("%s does not exit,spj src will will be compiled")
+				self.compile_spj(spj_version=spj_version,src=spj_src,
+								spj_compile_config=spj_compile_config)
+		with InitSubmissionEnv(JUDGER_WORKSPACE_BASE,submission_id=str(submission_id)) as submission_dir:
+				if compile_config:
+					src_path=os.path.join(submission_dir,compile_config["src_name"])
+					with open(src_path,"w") as f:
+						f.write(serc.encode("utf-8"))
+
+				exe_path=Compiler().compile(compile_config=compile_config,
+											src_path=src_path,
+											output_dir=submission_dir)
+				else:
+					exe_path=os.path.join(submission_dir,run_config["exe_name"])
+					with open(exe_path,"w") as f:
+						w.write(src.encode("utf-8"))
+
 
 	def pong(self):
 		data=server_info()
