78e42388a0585597cdf5cb30b04a9adc3b07f68c
sanWenyua <mrkangone@hotmail.com>
2018-01-09 13:13:13 +0800

    edit server.py

--- judger_server/server.py
+++ judger_server/server.py
@@ -9,13 +9,37 @@ urls=("/judge","JudgerServer",
 		"/ping","JudgerServer",
 		"/spj_judge","JudgerServer")
 
+class InitSubmissionEnv(object):
+	def __init__(self,judger_workspace,submission_id):
+		self.path=os.path.join(judger_workspace,submission_id)
+
+	def __entry__(self):
+		try:
+			os.mkdir(self.path)
+			os.chmod(self.path,0777)
+		except Exception as e:
+			logger.exception(e)
+			raise JudgerClientError("faild to clean the runtime env")
+
+	def __exit__(self,exc_type,exc_val,exc_tb):
+		if not DEBUG:
+			try:
+				shutil.rmtree(self.tree)
+			except Exception as e:
+				logger.exception(e)
+				raise JudgerClientError("fail to clean the dir")
+
+
+
 class JudgerServer(object):
 	def judge(self):
+
 	def pong(self):
 		data=server_info()
 		data["action"]="pong"  #add element
 		return data
 	def spj_judge(self):
+
 	def POST(self):
 		_token=web.ctx.env.get("HTTP_X_JUDGE_SERVER_TOKEN",None)
 
