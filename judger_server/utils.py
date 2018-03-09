6921382bff247cdc1dff086016f2724f365744ee
sanWenyua <mrkangone@hotmail.com>
2018-03-09 22:26:59 +0800

    fix the problem that text formatting error in utils.py

--- judger_server/utils.py
+++ judger_server/utils.py
@@ -17,11 +17,11 @@ logger.setLevel(logging.WARMMING)
 def server_info():
 	version=_judger.VERSION
 	return {"hostname":socket.gethostname(),
-			"cpu":psutil.cpu_percent(),
-			"cpu_core":psutil.cpu_count(),
-			"memory":psutil.virtual_memory().percent,
-			"version":".".join([str((version>>16)&0xff),str((version>>8)&0xff),str(version&0xff)])
-			}
+		"cpu":psutil.cpu_percent(),
+		"cpu_core":psutil.cpu_count(),
+		"memory":psutil.virtual_memory().percent,
+		"version":".".join([str((version>>16)&0xff),str((version>>8)&0xff),str(version&0xff)])
+		}
 
 def get_token():
 	token=os.environ.get("TOKEN")
