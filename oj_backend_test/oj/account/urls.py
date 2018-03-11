ff04281db91fc26b3826e12179f0573b0e3e1da1
sanWenyua <mrkangone@hotmail.com>
2018-03-11 05:58:31 -0700

    fix problem about account/urls.py

--- oj_backend_test/oj/account/urls.py
+++ oj_backend_test/oj/account/urls.py
@@ -6,7 +6,7 @@ urlpatterns=[
 	path("",views.ping,name="ping"),
 	#path("<int:question_id>/",views.detail,name="detail"),
 	#after modify
-	path("<int:question>/",views.detaild,name="index"),
+	path("<int:question>/",views.detail,name="index"),
 	path("<int:question>/results/",views.results,name="results"),
 	path("<int:question_id>/vote/",views.vote,name="vote"),
 	path("index/",views.index,name="index"),
