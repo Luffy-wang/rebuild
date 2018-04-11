46561f1a1f426ae586ebc929b52e0d30c623e90a
sanWenyua <mrkangone@hotmail.com>
2018-04-11 13:43:39 +0800

    test ci

--- test.py
+++ test.py
@@ -1,19 +1,23 @@
-import django
+# import django
 from django.test.utils import setup_test_environment
 from django.test import Client
-from django.urls import reverse
-import os
+# from django.urls import reverse
+# import os
 
+#os.system("echo setup_test_environment() > python manage.py shell")
 
+setup_test_environment()
+client=Client()
+response=client.get("/account/loginorlogout")
+response.status_code
 
+# def test():
 
-def test():
+#     setup_test_environment()
+#     client=Client()
+#     response=client.get("account/loginorlogout")
+#     response.status_code
+#     if response.status_code==200:
+#         return 0
 
-    setup_test_environment()
-    client=Client()
-    response=client.get("account/loginorlogout")
-    response.status_code
-    if response.status_code==200:
-        return 0
-
-test()
+# test()
